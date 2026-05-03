f36f6f720216716943f8531a90d6b0a9ee83792e
Hari <vhariprasathbsc@gmail.com>
2026-05-03 11:31:48 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,9 +6,7 @@ const jwt = require("jsonwebtoken");
 
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    service: "gmail",
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
